4cbf877b639e4412222ea805d20e5a724d4bd72d
dicky.c <dicky.c@binance.com>
2023-08-21 17:28:49 +0800

    feat: more setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App(): JSX.Element {
-  return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center text-white bg-coolGray-800 text-3xl">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="text-blue-200" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,12 @@
-const colors = require('tailwindcss/colors');
-
-module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: 'class', // or 'media' or false
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    colors: colors,
-    screens: {
-      sm: '576px',
-      md: '768px',
-      lg: '992px',
-      xl: '1200px',
-      xxl: '1400px',
-    },
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/forms')],
-};
+  plugins: [],
+}
+
